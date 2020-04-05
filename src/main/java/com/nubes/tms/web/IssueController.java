dd6f98cdcd9a890541a591255b8e1dbc7d6ed951
M Rajesh <m.rajeshrajz@gmail.com>
2020-04-05 19:33:28 +0530

    Refactored Ticket to issue

--- src/main/java/com/nubes/tms/web/IssueController.java
+++ src/main/java/com/nubes/tms/web/IssueController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nubes.tms.domain.Issue;
-import com.nubes.tms.service.IssueTicketService;
+import com.nubes.tms.service.IssueService;
 
 import java.util.List;
 
@@ -23,45 +24,44 @@ public class IssueController {
 	private static final Logger Log = LoggerFactory.getLogger(IssueController.class);
 
 	@Autowired
-	private IssueTicketService issueTicketService;
+	private IssueService issueTicketService;
 
 	@PostMapping("/new")
 	public Issue newIssue(@RequestBody Issue issue) {
 		Log.info("New Issue for module {} ", issue.getModule());
-		return issueTicketService.issueNewTicket(issue);
+		return issueTicketService.createNewIssue(issue);
 	}
 
-	@GetMapping("/get")
+	@GetMapping("/all")
 	public List<Issue> getAllIssues() {
-		Log.info("get all issues");
 		return issueTicketService.getAllIssues();
 	}
 
-	@GetMapping("/get/{id}")
+	@GetMapping("{id}")
 	public Issue getIssue(@PathVariable String id) {
 		Log.info("Searching for issue with id {} ", id);
-		return issueTicketService.getTicketById(id);
+		return issueTicketService.getIssueById(id);
 	}
 
-	@GetMapping("/get/bymodule/{moduleName}")
+	@GetMapping("module/{moduleName}")
 	public List<Issue> getAllIssuesByModule(@PathVariable String moduleName) {
 		Log.info("retriving all issues with module name {} ", moduleName);
-		return issueTicketService.getAllIssuesByModule(moduleName);
+		return issueTicketService.getIssuesByModule(moduleName);
 	}
 
-	@GetMapping("/get/bystatus/{status}")
+	@GetMapping("status/{status}")
 	public List<Issue> getAllIssuesByStatus(@PathVariable String status) {
 		Log.info("retriving all issues with status  {} ", status);
-		return issueTicketService.getAllIssuesByStatus(status);
+		return issueTicketService.getIssuesByStatus(status);
 	}
 
-	@GetMapping("/get/bypriority/{priority}")
+	@GetMapping("priority/{priority}")
 	public List<Issue> getAllIssuesByPriority(@PathVariable String priority) {
 		Log.info("retriving all issues with Priority  {} ", priority);
-		return issueTicketService.getAllIssuesByPriority(priority);
+		return issueTicketService.getIssuesByPriority(priority);
 	}
 
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public Issue updateIssue(@RequestBody Issue issue) {
 		Log.info("new update request for problem statement {} and status {} ", issue.getProblemStatement(),
 				issue.getStatus());
