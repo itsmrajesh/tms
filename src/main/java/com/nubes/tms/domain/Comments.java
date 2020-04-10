b99389ddae56d343791ff26bc7e6544462b52dfc
M Rajesh <m.rajeshrajz@gmail.com>
2020-04-10 10:27:10 +0530

    removed unused imports

--- src/main/java/com/nubes/tms/domain/Comments.java
+++ src/main/java/com/nubes/tms/domain/Comments.java
@@ -1,10 +1,6 @@
 package com.nubes.tms.domain;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.domain.AuditorAware;
 
 import lombok.Getter;
 import lombok.Setter;
